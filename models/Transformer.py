2ef7b9bf0384bf65cf3c97ddd100649c32318e81
WangHr <WangHr@cug.edu.cn>
2023-02-04 09:34:26 +0800

    Bug Fix

--- models/Transformer.py
+++ models/Transformer.py
@@ -1,14 +1,10 @@
 import torch
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
 
 from torch import nn
 from torch import Tensor
-from PIL import Image
-from torchvision.transforms import Compose, Resize, ToTensor
-from einops import rearrange, reduce, repeat
-from einops.layers.torch import Rearrange, Reduce
-from torchsummary import summary
+from einops import rearrange
+from einops.layers.torch import Rearrange
 
 
 class PatchEmbedding(nn.Module):
